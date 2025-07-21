5d99a6e9ddd0b1cd1a0f055236235b32a40b41d4
Galina Ponomareva <li.c@mail.ru>
2025-07-21 23:22:56 +0300

    параметризированный тест

---
+++ src/test/java/li/c/ParameterizedTests.java
@@ -0,0 +1,54 @@
+package li.c;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Практика с параметризированными тестами")
+
+public class ParameterizedTests {
+    @BeforeEach
+    void beforeEach() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @DisplayName("Проверка поиска на GitHub")
+    @ParameterizedTest(name = "В выдаче результатов на первом месте должен отображаться {0}")
+    @ValueSource(strings = {"junit5", "selenide"})
+    void firstSearchResultShouldContainExpectedTest(String testData) {
+        open("https://github.com/");
+        $("[placeholder='Search or jump to...']").click();
+        $("#query-builder-test").setValue(testData).pressEnter();
+        $$("[data-testid='results-list'] div").first().shouldHave(text(testData));
+
+    }
+
+
+    @DisplayName("Проверка регистрации на demoqa.com")
+    @ParameterizedTest(name = "В поле вывода результатов регистрации должны отображаться {0}, {1}, {2}, {3}")
+    @CsvSource({
+            "Alex Egorov, alex@egorov.com, Some address 1, Another address 1",
+            "Ivan Semenov, ivan@semenov.com, Some address 3, Another address 4"
+    })
+    void fillFormTest(String testName, String testEmail, String currentAddress, String permanentAddress) {
+        open("https://demoqa.com/text-box");
+
+        $("#userName").setValue(testName);
+        $("#userEmail").setValue(testEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        $("#output").shouldHave(text(testName), text(testEmail),
+                text(currentAddress), text(permanentAddress));
+    }
+
+}
